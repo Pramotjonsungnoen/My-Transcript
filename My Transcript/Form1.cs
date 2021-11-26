7520176edb463b65b00a7864fccc69116426e41f
pramot <pramot112@gmail.com>
2021-11-26 17:30:13 +0700

    commit3

--- My Transcript/Form1.cs
+++ My Transcript/Form1.cs
@@ -12,9 +12,13 @@ namespace My_Transcript
 {
     public partial class Form1 : Form
     {
+        List<Course> year1Sem1;
+        List<Course> year1Sem2;
         public Form1()
         {
             InitializeComponent();
+            year1Sem1 = new List<Course>();
+            year1Sem2 = new List<Course>();
         }
         private void label1_Click(object sender, EventArgs e)
         {
@@ -48,6 +52,26 @@ namespace My_Transcript
             newCourse.CourseName = textBoxCourseName.Text;
             newCourse.CourseCredit = textBoxCourseID.Text;
             newCourse.Grade = textBoxGrade.Text;
+
+            switch (tabControl1.SelectedIndex)
+            {
+                case 0:
+                    dataGridViewY1S1.DataSource = null;
+                    year1Sem1.Add(newCourse);
+                    dataGridViewY1S1.DataSource = year1Sem1;
+                    break;
+                case 1:
+                    dataGridViewY1S2.DataSource = null;
+                    year1Sem2.Add(newCourse);
+                    dataGridViewY1S2.DataSource = year1Sem2;
+                    break;
+            }
+        
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
     class Course
